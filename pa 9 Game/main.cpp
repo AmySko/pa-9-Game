c6e0bb1e1fc46d9de2c1da4c8c6cc8665c482e99
AmySko <38385242+AmySko@users.noreply.github.com>
2018-04-14 14:19:02 -0700

    Made a red background

--- pa 9 Game/main.cpp
+++ pa 9 Game/main.cpp
@@ -7,19 +7,25 @@ influences:
 	The beauty that is the internet
 	SFML website
 	Pong lecture/code
-
+	The glory that is stackoverflow
 
 */
 
 
 #include <SFML/Graphics.hpp> 
 #include "Table.h"
+#include <windows.h> //for color
+
 
 int main(void)
 {
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	Table t1(*(new sf::Vector2f(20, 15)), sf::Color::White, *(new sf::Vector2f(window.getSize().x*.05, window.getSize().y*.7)));
-	
+	Table background(*(new sf::Vector2f(0, 0)), sf::Color::Red, *(new sf::Vector2f(window.getSize().x*1, window.getSize().y*1)));
+
+	/*HANDLE hConsole;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, 211);*/
 	//sf::CircleShape shape(100.f);
 	//shape.setFillColor(sf::Color::Green);
 
@@ -28,12 +34,14 @@ int main(void)
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
+			
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
 
 		window.clear();
 		//window.draw(shape);
+		window.draw(background);
 		window.draw(t1);
 		window.display();
 	}
