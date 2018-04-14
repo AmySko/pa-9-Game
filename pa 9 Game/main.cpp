664f2cfca62b30e1933e8ca0fb412b764aa744ee
AmySko <38385242+AmySko@users.noreply.github.com>
2018-04-14 14:45:52 -0700

    Starting to put in chairs
    chairs are not equa distance from each other

--- pa 9 Game/main.cpp
+++ pa 9 Game/main.cpp
@@ -20,8 +20,18 @@ influences:
 int main(void)
 {
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	Table t1(*(new sf::Vector2f(20, 15)), sf::Color::White, *(new sf::Vector2f(window.getSize().x*.05, window.getSize().y*.7)));
+	Table t1(*(new sf::Vector2f(70, 15)), sf::Color::White, *(new sf::Vector2f(window.getSize().x*.05, window.getSize().y*.8)));
+	Table counter(*(new sf::Vector2f(190, 15)), sf::Color::White, *(new sf::Vector2f(window.getSize().x*.05, window.getSize().y*.7)));
 	Table background(*(new sf::Vector2f(0, 0)), sf::Color::Red, *(new sf::Vector2f(window.getSize().x*1, window.getSize().y*1)));
+	
+	Table chair1(*(new sf::Vector2f(60, 15)), sf::Color::Yellow, *(new sf::Vector2f(window.getSize().x*.04, window.getSize().y*.05)));
+	Table chair2(*(new sf::Vector2f(60, 35)), sf::Color::Yellow, *(new sf::Vector2f(window.getSize().x*.04, window.getSize().y*.05)));
+	Table chair3(*(new sf::Vector2f(60, 55)), sf::Color::Yellow, *(new sf::Vector2f(window.getSize().x*.04, window.getSize().y*.05)));
+	Table chair4(*(new sf::Vector2f(60, 75)), sf::Color::Yellow, *(new sf::Vector2f(window.getSize().x*.04, window.getSize().y*.05)));
+	Table chair5(*(new sf::Vector2f(60, 100)), sf::Color::Yellow, *(new sf::Vector2f(window.getSize().x*.04, window.getSize().y*.05)));
+	Table chair6(*(new sf::Vector2f(60, 120)), sf::Color::Yellow, *(new sf::Vector2f(window.getSize().x*.04, window.getSize().y*.05)));
+	Table chair7(*(new sf::Vector2f(60, 143)), sf::Color::Yellow, *(new sf::Vector2f(window.getSize().x*.04, window.getSize().y*.05)));
+	Table chair8(*(new sf::Vector2f(60, 165)), sf::Color::Yellow, *(new sf::Vector2f(window.getSize().x*.04, window.getSize().y*.05)));
 
 	/*HANDLE hConsole;
 	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -43,6 +53,15 @@ int main(void)
 		//window.draw(shape);
 		window.draw(background);
 		window.draw(t1);
+		window.draw(counter);
+		window.draw(chair1);
+		window.draw(chair2);
+		window.draw(chair3);
+		window.draw(chair4);
+		window.draw(chair5);
+		window.draw(chair6);
+		window.draw(chair7);
+		window.draw(chair8);
 		window.display();
 	}
 	
