30875b13ce3f2eeced18037a020b9e548033ffc8
AmySko <38385242+AmySko@users.noreply.github.com>
2018-04-15 04:33:38 -0700

    comments, starting texture, weird shit

--- pa 9 Game/main.cpp
+++ pa 9 Game/main.cpp
@@ -8,7 +8,7 @@ influences:
 	SFML website
 	Pong lecture/code
 	The glory that is stackoverflow
-
+	The light that guides us home: cplusplus.com
 */
 
 
@@ -21,10 +21,15 @@ influences:
 
 int main(void)
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!"); //This changes the window size that pops up, do we want to chang it?
+
 	Table t1(*(new sf::Vector2f(70, 15)), sf::Color::White, *(new sf::Vector2f(window.getSize().x*.05, window.getSize().y*.8)));
 	Table counter(*(new sf::Vector2f(190, 15)), sf::Color::White, *(new sf::Vector2f(window.getSize().x*.05, window.getSize().y*.7)));
-	Table background(*(new sf::Vector2f(0, 0)), sf::Color::Red, *(new sf::Vector2f(window.getSize().x*1, window.getSize().y*1)));
+
+
+	//background should probably be changed from a table class
+	//Table background(*(new sf::Vector2f(0, 0)), sf::Color::Red, *(new sf::Vector2f(window.getSize().x*1, window.getSize().y*1)));
+	sf::Texture background;
 
 	//may decide to make a seperate chair class at some point
 	Table chair1(*(new sf::Vector2f(60, 15)), sf::Color::Yellow, *(new sf::Vector2f(window.getSize().x*.04, window.getSize().y*.05)));
@@ -42,11 +47,24 @@ int main(void)
 	sf::Time cook; //takes to cook
 	sf::Time walkout; //how long Andy is willing to wait
 
-	/*HANDLE hConsole;
-	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleTextAttribute(hConsole, 211);*/
+	/*This is code I found on cplusplus for setting an image 
+	for a sprite*/
+	/*.
+  sf::Texture imageSource;
+  if(!imageSource.loadFromFile("image.png"))
+      return EXIT_FAILURE;
+  sf::Sprite imageSprite;
+  imageSprite.setTexture(imageSource);*/
+
 	//sf::CircleShape shape(100.f);
 	//shape.setFillColor(sf::Color::Green);
+
+	//does weird shit to the background
+	//if(!background.loadFromFile("red and white tile.jpg"))
+	{
+		//cout << "background unable to load" << endl;
+		//return 0;
+	}
 
 	while (window.isOpen())
 	{
@@ -60,7 +78,7 @@ int main(void)
 
 		window.clear();
 		//window.draw(shape);
-		window.draw(background);
+		//window.draw(background);
 		window.draw(t1);
 		window.draw(counter);
 		window.draw(chair1);
